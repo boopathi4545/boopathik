059ad4689edc36bcc3d3e9720d504a7ce3c6e831
boopathi4545 <31505777+boopathi4545@users.noreply.github.com>
2018-01-09 10:54:28 +0000

    Create posnag.c

---
+++ posnag.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("ENTER THE NUMBER");
+scanf("%d",&n);
+if(n>0)
+printf("%d" a number is positive");
+elseif(n<0)
+printf("%d" a number is negative");
+else
+printf("%d" a number is zero");
+}
